57aabe1b33338463b5bb1ed0fc60da6806f74cf2
Dovatar <dovator@mail.ru>
2019-12-17 23:35:54 +0300

    TMS-JS

---
+++ JS/script/lesson.js
@@ -0,0 +1,39 @@
+//Задание 1
+let manager;
+let name;
+let currentUser;
+name = "Вася";
+manager = name;
+currentUser = manager;
+console.log(currentUser);
+//Задание 2
+let name = "Вася";
+console.log( `hello ${1}` );
+console.log( `hello ${"name"}` );
+console.log( `hello ${name}` );
+//Задание 3
+console.log(" \n \n" - 2); //-2
+console.log(true + false);// 1
+console.log(false + false); //0
+console.log(6 / "3");// 2
+console.log("2" * "3"); // 6
+console.log(7 / 0); //Infinity
+console.log(4 + 5 + "px"); //"9px"
+console.log("" + 3 + 0);// "30"
+console.log("" - 4 + 0);// -4
+console.log("!" + 2 + 5); //"!25"
+console.log("1" - 2); //-1
+console.log("3px" - 2); //NaN
+console.log("  -9  " + 5); //"-9 5"
+console.log("  -9  " - 5);// -14
+console.log(null + 1); //1
+console.log(undefined + 1);// NaN
+//Задание 4
+console.log(2 > 4);//false
+console.log(undefined == null);//true
+console.log(undefined === null);//false
+console.log(false == 0);//true
+console.log(false == "");//true
+console.log(false === "");//false
+//Задание 5
+
